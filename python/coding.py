bdf847251836094d6d87b5bb6f15d7d7a91361a6
Asaolu Rachael <asaolurachaelariom@gmail.com>
2024-09-28 12:47:35 +0100

    coding

---
+++ python/coding.py
@@ -0,0 +1,12 @@
+chatgpt = 18
+
+
+if chatgpt >= 18:
+    print("You are not eligible")
+    print("This is for student")
+elif chatgpt <= 18:
+    print("Welcome to chatgpt")
+    print("Hi Students")
+elif chatgpt >= 4 or chatgpt <= 18:
+    print("Welcome to chatgpt")
+    print("Hi students")
